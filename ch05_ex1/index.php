666e7162bdfe3afc496cea7ad8f87b1a854d4527
Aidan O'Neill <75544718+ONeillWD@users.noreply.github.com>
2021-09-09 13:02:35 -0400

    Init commit

---
+++ ch05_ex1/index.php
@@ -0,0 +1,15 @@
+<?php include 'view/header.php'; ?>
+<link rel="stylesheet" type="text/css"
+          href="main.css">
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Product Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
